e49c39a2833df1c0f971a5ab1a2225fecaa693f0
joevtap <joelvitortorres@gmail.com>
2022-03-22 12:10:29 -0300

    Added class transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +10,29 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: MyHomePage(),
+    return MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis',
+      value: 340.12,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Novo Celular',
+      value: 1190.12,
+      date: DateTime.now(),
+    ),
+  ];
+
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +40,19 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Despesas pessoais'),
       ),
-      body: const Center(
-        child: Text('Hello world'),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const <Widget>[
+            Card(
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+            Card(
+              child: Text('Lista de transações'),
+              elevation: 5,
+            ),
+          ]),
     );
   }
 }
